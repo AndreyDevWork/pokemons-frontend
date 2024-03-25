c3c7ed8b34d5fd9a7258fe07eed8a00b0576b54b
Andrey <andrey.develop.a@gmail.com>
2024-03-26 03:38:48 +0700

    build: prettier

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1 class="text-3xl text-amber-300 font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <p>{{ counter }}</p>
+    <button @click="increaseCounter">Increase Counter</button>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const increaseCounter = () => {
+  counter.value++;
+};
+
+const message = "Hello, Vue!";
+</script>
