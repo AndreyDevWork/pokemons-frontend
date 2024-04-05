9a84bbc0c441f9c5c270668f1594f8f58292dccd
Andrey <andrey.develop.a@gmail.com>
2024-04-06 04:29:02 +0700

    feat: i18n

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,7 @@ import './styles/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
@@ -12,8 +13,14 @@ import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
 
-app.use(createPinia()).use(router).use(Quasar, {
-  plugins: {} // import Quasar plugins and add here
-})
+const i18n = createI18n({})
+
+app
+  .use(createPinia())
+  .use(router)
+  .use(Quasar, {
+    plugins: {} // import Quasar plugins and add here
+  })
+  .use(i18n)
 
 app.mount('#app')
--- src/pages/Auth/SignUpPage.vue
+++ src/pages/Auth/SignUpPage.vue
@@ -1,15 +1,16 @@
 <template>
-  <div><h1>Sign dddUp</h1></div>
-  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-    <div class="flex">
-      <div class="text-xl font-medium text-black">ChitChat</div>
-      <p class="text-slate-500">You have a new message!</p>
-    </div>
+  <div style="max-width: 360px" class="mx-auto">
+    <h1 class="text-4xl">Sign-Up</h1>
+    <q-input v-model="username" class="mt-3" filled color="black" outlined label="Username" />
+    <q-input v-model="password" class="mt-3" filled color="black" outlined label="Password" />
   </div>
 </template>
 
 <script setup lang="ts">
-console.log(import.meta.env.VITE_API_URL)
+import { ref } from 'vue'
+
+const username = ref()
+const password = ref()
 </script>
 
 <style scoped></style>
