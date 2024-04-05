790db70643ddc07203caa2da64bb319053fa8428
Andrey <andrey.develop.a@gmail.com>
2024-04-06 01:48:04 +0700

    build: add quasar

--- src/app/main.ts
+++ src/app/main.ts
@@ -6,9 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router).use(Quasar, {
+  plugins: {} // import Quasar plugins and add here
+})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    VueDevTools(),
+    quasar({
+      sassVariables: './src/app/quasar-variables.sass'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
