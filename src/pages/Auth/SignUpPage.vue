2691208f5ea65a76c390d56f63b3b787774bfede
Andrey <andrey.develop.a@gmail.com>
2024-04-06 03:39:36 +0700

    feat: sign up

---
+++ src/pages/Auth/SignUpPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><h1>Sign dddUp</h1></div>
+  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div class="flex">
+      <div class="text-xl font-medium text-black">ChitChat</div>
+      <p class="text-slate-500">You have a new message!</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+console.log(import.meta.env.BASE_URL)
+</script>
+
+<style scoped></style>
