b67cbd7fd7e31d5ba1774a947099c90559a7c78c
Andrey <andrey.develop.a@gmail.com>
2024-04-07 09:48:04 +0700

    feat: header

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DefaultLayout from '@/shared/ui/layouts/DefaultLayout.vue'
+</script>
 
 <template>
-  <RouterView />
+  <DefaultLayout />
 </template>
