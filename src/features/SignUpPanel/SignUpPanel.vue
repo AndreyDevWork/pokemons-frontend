e575a00a18682289c74a08834ef66cd662640c52
Andrey <andrey.develop.a@gmail.com>
2024-04-13 15:50:06 +0700

    feat: errors

--- src/features/SignUpPanel/SignUpPanel.vue
+++ src/features/SignUpPanel/SignUpPanel.vue
@@ -8,16 +8,26 @@ const signUpData = reactive<SignUpRequest>({
   password: ''
 })
 
-const signUp = async () => {
-  spinner.value = true
-  await AuthService.signUp(signUpData)
-  spinner.value = false
-}
-
 const spinner = ref(false)
+const error = ref(false)
+
+const signUp = () => {
+  spinner.value = true
+  AuthService.signUp(signUpData)
+    .then(() => {
+      error.value = false
+    })
+    .catch(({ response }) => {
+      error.value = response.data.message
+      console.log(error.value)
+    })
+    .finally(() => {
+      spinner.value = false
+    })
+}
 </script>
 <template>
-  <div style="max-width: 400px" class="mx-auto px-5 pt-8 pb-16 rounded-lg bg-white">
+  <div style="max-width: 400px" class="mx-auto px-5 pt-8 pb-10 rounded-lg bg-white">
     <h1 class="text-4xl">{{ $t('CreateAccountOnFluentFlow') }}</h1>
     <q-input
       v-model="signUpData.username"
@@ -39,9 +49,13 @@ const spinner = ref(false)
       color="black"
       class="full-width h-14 mt-3"
       :label="spinner ? '' : $t('create')"
+      :disable="spinner"
       @click="signUp"
     >
       <q-spinner-facebook v-if="spinner" color="white" size="1.8em" />
     </q-btn>
+    <q-banner v-if="error" rounded inline-actions class="text-white mt-3 bg-rose-800">
+      {{ error }}
+    </q-banner>
   </div>
 </template>
--- src/pages/Auth/SignUpPage/SignUpPage.vue
+++ src/pages/Auth/SignUpPage/SignUpPage.vue
@@ -1,7 +1,7 @@
-<template>
-  <SignUpPanel />
-</template>
-
 <script setup lang="ts">
 import { SignUpPanel } from '@/features/SignUpPanel'
 </script>
+
+<template>
+  <SignUpPanel />
+</template>
