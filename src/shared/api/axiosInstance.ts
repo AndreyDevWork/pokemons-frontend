3b0b6d624afb779f33e19163fd47b8f1b47943b4
Andrey <andrey.develop.a@gmail.com>
2024-04-13 14:23:45 +0700

    feat: sign up

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -5,9 +5,9 @@ const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
 
-// api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-//   config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-//   return config
-// })
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+  return config
+})
 
 export default api
