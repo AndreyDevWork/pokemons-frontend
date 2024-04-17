579f74994d36cd7831e940e894bb0cdb9e0df936
Андрей Охота <sssmetannikov.a@gmail.com>
2024-04-18 00:53:50 +0700

    feat: signIn route

---
+++ src/pages/Auth/SignInPage/SignInPage.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Вход</div>
+</template>
---
+++ src/pages/Auth/SignInPage/index.ts
@@ -0,0 +1,2 @@
+import SignInPage from '@/pages/Auth/SignInPage/SignInPage.vue'
+export default SignInPage
